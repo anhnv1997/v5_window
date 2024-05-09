ece52b6c19bcd13a81077a866f3ddb06fccffafb
anhnv1997 <84891047+anhnv1997@users.noreply.github.com>
2024-05-09 11:49:13 +0700

    init

--- iParking.ConfigurationManager/obj/x64/Debug/net6.0-windows/iParking.ConfigurationManager.AssemblyInfo.cs
+++ iParking.ConfigurationManager/obj/x64/Debug/net6.0-windows/iParking.ConfigurationManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iParking.ConfigurationManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1+eb550dcea84bb3db63d069e6d3e10f7e3a3034e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("iParking.ConfigurationManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iParking.ConfigurationManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.1")]
